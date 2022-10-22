c350f788c127364fc18bd5d259eeb94a79c936c3
BISMA1922 <bismaalifia@gmail.com>
2022-10-22 10:30:34 +0700

    commit pertama

---
+++ src/model/model_barang.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.SQLException;
+import view.v_barang;
+
+/**
+ *
+ * @author asus
+ */
+public class model_barang implements controller.c_barang{
+
+    @Override
+    public void Cari(v_barang vb) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Tambah(v_barang vb) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Ubah(v_barang vb) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Hapus(v_barang vb) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Keluar(v_barang vb) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
